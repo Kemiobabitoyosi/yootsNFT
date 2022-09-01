4834c89b2c36ce858091ddb4084b896e2844a04c
Kemiobabitoyosi <kemiobabi.toyosi@gmail.com>
2022-09-01 18:09:49 +0100

    Yoots Nft

---
+++ yoots.js
@@ -0,0 +1,9 @@
+export const Demo = () => {
+  return (
+      <div className='container'>
+        <div className='pic' id='pic3' />
+        <div className='pic' id='pic2' />
+        <div className='pic' id='pic1' />
+      </div>
+  );
+};
